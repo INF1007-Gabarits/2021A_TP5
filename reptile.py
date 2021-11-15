7c7218b337e641755d6d8ea812e945a1221692a4
Philippe Rivest <technophil98@gmail.com>
2021-11-15 01:28:28 -0500

    Initial commit

---
+++ reptile.py
@@ -0,0 +1,37 @@
+from abc import ABC
+
+from animal import Animal
+
+
+# TODO Je suis abstraite et j'hérite de Animal
+class Reptile():
+
+    # TODO Implantez mon constructeur
+    #  def __init__(self, ...) -> None:
+
+
+    def __str__(self) -> str:
+        # TODO Je dois retournez une chaine de caractère semblable à :
+        #  Le TYPE_REPTILE NOM_REPTILE est nocturne.
+        #  ou
+        #  Le TYPE_REPTILE NOM_REPTILE n'est pas nocturne.
+        pass
+
+
+# TODO J'hérite de Reptile
+class Serpent():
+
+    # TODO Implantez mon constructeur
+    #  def __init__(self, ...) -> None:
+
+    def __str__(self) -> str:
+        # TODO Ajouter les phrases suivantes à la chaine de base de Reptile:
+        #  Il est venimeux.
+        #  ou
+        #  Il n'est pas venimeux.
+        #  Utilisez la methode __str__ de Reptile avec: super(Serpent, self).__str__()
+        pass
+
+    def crier(self) -> str:
+        # TODO Retournez le cri du serpent: sssss
+        pass
